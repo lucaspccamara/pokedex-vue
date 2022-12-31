bd74345a4ebabfa90b1e06ef70f6cba81ad9e5cd
lucaspccamara <lucaspatrickccamara@gmail.com>
2022-12-31 17:06:35 -0300

    Add background and sound track

---
+++ pokedex-app/src/components/MyPokedex.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-row justify="end">
+        <v-col cols="3">
+            <audio controls loop>
+                <source src="../assets/sounds/background-song.wav" type="audio/wav">
+            </audio>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
