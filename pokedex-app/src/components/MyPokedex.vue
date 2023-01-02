ccbaea8fe8d07f0d5ff979779061fedc41baf6fa
lucaspccamara <lucaspatrickccamara@gmail.com>
2023-01-01 22:36:27 -0300

    Pokedex template

--- pokedex-app/src/components/MyPokedex.vue
+++ pokedex-app/src/components/MyPokedex.vue
@@ -1,11 +1,20 @@
 <template>
     <v-row justify="end">
-        <v-col cols="3">
-            <audio controls loop>
-                <source src="../assets/sounds/background-song.wav" type="audio/wav">
-            </audio>
-        </v-col>
-    </v-row>
+            <v-col cols="3">
+                <audio controls loop>
+                    <source src="../assets/sounds/background-song.wav" type="audio/wav">
+                </audio>
+            </v-col>
+        </v-row>
+    <div class="container">
+        <div class="poke-detail">
+
+        </div>
+        <div class="poke-list">
+
+        </div>
+        <img class="bg-pokedex" src="../../src/assets/pokedex.png">
+    </div>
 </template>
 
 <script>
@@ -14,6 +23,63 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.bg-pokedex {
+    max-width: 100%;
+    max-height: 100%;
+}
 
+.container {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+
+.poke-list {
+    position: absolute;
+    margin-left: 35%;
+    margin: 4px, 4px;
+    padding: 4px;
+    width: 280px;
+    height: 380px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    text-align: justify;
+
+    /* Hide scrollbar for IE, Edge and Firefox */
+    -ms-overflow-style: none;
+    /* IE and Edge */
+    scrollbar-width: none;
+    /* Firefox */
+}
+
+.poke-list::-webkit-scrollbar {
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    display: none;
+}
+
+.poke-detail {
+    position: absolute;
+    margin-right: 28%;
+    margin-top: 8%;
+    //  margin:4px, 4px;
+    //  padding:4px;
+    width: 375px;
+    height: 530px;
+    border-radius: 5px;
+    overflow-x: hidden;
+    overflow-y: auto;
+
+    /* Hide scrollbar for IE, Edge and Firefox */
+    -ms-overflow-style: none;
+    /* IE and Edge */
+    scrollbar-width: none;
+    /* Firefox */
+}
+
+.poke-detail::-webkit-scrollbar {
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    display: none;
+}
 </style>
